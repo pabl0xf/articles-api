428a6e13b3c347e8f36fff193d164a7aff75af53
Pablo Frias <frias.pablo@gmail.com>
2018-08-26 19:06:28 -0300

    Add update and delete article

--- app/controllers/article.controller.js
+++ app/controllers/article.controller.js
@@ -23,3 +23,48 @@ exports.create = (req, res) => {
         });
     });
 };
+
+exports.update = (req, res) => {
+  if(!req.body.text) {
+    return res.status(400).send({
+      message: "Article text can not be empty"
+    });
+  }
+
+  Article.findByIdAndUpdate(req.params.articleId, {
+    title: req.body.title || "Untitled article",
+    text: req.body.text,
+    tags: req.body.tags,
+    userId: 1
+  }, {new: true})
+  .then(article => {
+    if(!article) {
+      return res.status(404).send({ message: "article not found with id " + req.params.articleId });
+    }
+
+    res.send(article);
+  }).catch(err => {
+    if(err.kind === 'ObjectId') {
+      return res.status(404).send({ message: "article not found with id " + req.params.articleId });
+    }
+
+    return res.status(500).send({ message: "Error updating article with id " + req.params.articleId });
+  });
+};
+
+exports.delete = (req, res) => {
+  Article.findByIdAndRemove(req.params.articleId)
+  .then(article => {
+    if(!article) {
+      return res.status(404).send({ message: "article not found with id " + req.params.articleId });
+    }
+
+    res.send({message: "article deleted successfully!"});
+  }).catch(err => {
+    if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+      return res.status(404).send({ message: "article not found with id " + req.params.articleId });
+    }
+
+    return res.status(500).send({ message: "Could not delete article with id " + req.params.articleId });
+  });
+};
